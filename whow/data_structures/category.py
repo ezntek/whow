7f954f46baec9b72ce1741f3e34b35d8d3113246
ezntek <ezntek@xflymusic.com>
2023-03-04 19:50:45 +0800

    intermetiate commit

--- whow/data_structures/category.py
+++ whow/data_structures/category.py
@@ -12,6 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+import os
 import dataclasses
 import typing
 import tomli_w as toml_writer
@@ -19,8 +20,6 @@ import tomli_w as toml_writer
 from .. import util
 from .. import exceptions
 from ..colors import colors, styles
-from ..config import *
-
 
 class CategoryTypedDict(typing.TypedDict):
     name: str
@@ -47,12 +46,12 @@ def from_dict(d: CategoryTypedDict) -> Category:
         colors.from_name(d["color"].lower())
     )
     
-def from_name(s: str, cfg: Config = Config()) -> Category:
+def from_name(s: str) -> Category:
     """
     Parse a dictionary that was parsed rom a `Category` back into a `Category`.
     """
 
-    BASEDIR = os.path.join(cfg.data_tree_dir, "categories")
+    BASEDIR = os.path.join(os.environ["HOME"], "./.local/categories")
     for category_filename in os.listdir(BASEDIR):
         with open(os.path.join(BASEDIR, category_filename), "rb") as f:
             c: CategoryTypedDict = toml_reader.load(f) # type: ignore
@@ -60,84 +59,83 @@ def from_name(s: str, cfg: Config = Config()) -> Category:
                 return Category(s, colors.from_name(c["color"]))
     raise NameError("The category name was not found! perhaps you did not add it yet?")
 
-def check_category_existence(name: str, cfg: Config = Config()) -> bool:
+def check_category_existence(name: str) -> bool:
     """
     Check for the existence of a given category by searching through the category directory.
     """
-    for path in os.listdir(os.path.join(cfg.data_tree_dir, f"categories")):
+    for path in os.listdir(os.path.join(os.environ["HOME"], "./.local/categories")):
         if (name == os.path.splitext(path)[0]) or (name.lower() == os.path.splitext(path)[0].lower()):
             return True
     return False
 
-def match_name_with_category(name: str, cfg: Config = Config()) -> Category:
+def match_name_with_category(name: str) -> Category:
     """
     Get a category class, given the name of an
     existing category.
     """
     
-    for path in os.listdir(os.path.join(cfg.data_tree_dir, f"categories")):
+    for path in os.listdir(os.path.join(os.environ["HOME"], "./.local/categories")):
         if os.path.splitext(path)[0] == name:
             try:
-                with open(os.path.join(cfg.data_tree_dir, "categories", path), "rb") as f:
+                with open(os.path.join(os.environ["HOME"], "./.local/categories", path), "rb") as f:
                     toml_dict: CategoryTypedDict = toml_reader.load(f) # type: ignore
-                    return from_name(toml_dict["name"], cfg)
+                    return from_name(toml_dict["name"])
             except NameError:
                 break
     raise NameError("The category name was not found! Perhaps you did not add it yet?")
 
-def match_category_name_with_filename(name: str, cfg: Config = Config()) -> str:
+def match_category_name_with_filename(name: str) -> str:
     """
     Find a filename based on the category name.
     """
     name = name.replace(" ", "_").lower()
 
-    for n in os.listdir(os.path.join(cfg.data_tree_dir, f"categories")):
+    for n in os.listdir(os.path.join(os.environ["HOME"], "./.local/categories")):
         if n == f"{name}.toml":
             return (n)
 
     raise NameError("No category found with name!")
 
-def register_category(category: Category, cfg: Config = Config(), force: bool = False, quiet: bool = False) -> typing.Union[str, None]:
+def register_category(category: Category, force: bool = False, quiet: bool = False) -> None:
     """
     Register a new category.
     """
     
-    n = category.name.replace(" ", "_")
-
-    if os.path.exists(os.path.join(cfg.data_tree_dir, f"categories/{n}.toml")):
+    if os.path.exists(os.path.join(os.environ["HOME"], "./.local/categories/{n}.toml")):
         if not force:
             if not quiet:
                 raise exceptions.FatalError("A category entry with the same name exists. Aborting...")
         util.warn("A category entry with the same name already exists. Overwriting...") if not quiet else None
 
-    with open(os.path.join(cfg.data_tree_dir, f'categories/{n}.toml'), "w+") as categorytoml:
-        t = toml_writer.dumps(dict(category.get_dict()))
-        categorytoml.write(t)
+    with open(os.path.join(os.environ["HOME"], './.local/categories/{n}.toml'), "wb") as f:
+        c_dict = dict(category.get_dict())
+        toml_writer.dump(c_dict, f)
 
-    return f"Wrote a new category toml. \n{t}" if not quiet else None
-
-def del_category(name: str, cfg: Config = Config()) -> str:
+def del_category(name: str) -> str:
     """
     Delete a category by its name.
     """
-    BASEDIR = os.path.join(os.path.join(cfg.data_tree_dir, f"categories"))
+    BASEDIR = os.path.join(os.path.join(os.environ["HOME"], "categories"))
     try:
-        filename = match_category_name_with_filename(name, cfg)
+        filename = match_category_name_with_filename(name)
         os.remove(os.path.join(BASEDIR, filename))
         return f"Deleted category: {name}"
     except NameError:
         util.error("A category with this name does not exist! please re-evaluate your input.")
     return ""
     
-def list_categories(cfg: Config = Config()) -> None:
+def get_categories_list() -> list[Category]:
     """
-    List all categories.
+    Get a list of all categories that exist.
     """
     
-    for path in os.listdir(os.path.join(cfg.data_tree_dir, "categories")):
+    retval: list[Category] = []
+    for path in os.listdir(os.path.join(os.environ["HOME"], "./.local/categories")):
         try:
-            with open(os.path.join(cfg.data_tree_dir, "categories", path), "rb") as f:
+            with open(os.path.join(os.environ["HOME"], "./.local/categories", path), "rb") as f:
                 toml_dict: CategoryTypedDict = toml_reader.load(f) # type: ignore
-                print(from_dict(toml_dict))
+                retval.append(from_dict(toml_dict))
         except KeyError or TypeError:
             util.warn(f"The category file {path} in the folder is corrupted!")
+        
+    return retval
